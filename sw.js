c14d8f093b21602a184b16685ae2da692cdda434
branzfamily01 <branz.family.01@gmail.com>
2026-08-15 17:54:45 +0900

    Bump cache for Firebase auth UI fix

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const CACHE="kaji-redesign-v2.1.1";
-const ASSETS=["./","./index.html","./style.css","./v21.css","./app.js","./firebase-config.js","./cloud-bridge.js","./manifest.webmanifest","./icon.svg"];
+const CACHE="kaji-redesign-v2.1.2";
+const ASSETS=["./","./index.html","./style.css","./v21.css","./app.js","./firebase-config.js","./cloud-bridge.js","./cloud-ui-fix.js","./manifest.webmanifest","./icon.svg"];
 self.addEventListener("install",e=>{e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)));self.skipWaiting()});
 self.addEventListener("activate",e=>{e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))));self.clients.claim()});
 self.addEventListener("fetch",e=>{if(e.request.method!=="GET")return;e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request).then(resp=>{const copy=resp.clone();caches.open(CACHE).then(c=>c.put(e.request,copy));return resp}).catch(()=>caches.match("./index.html"))))});
